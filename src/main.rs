402e11cb576397149e455be592aa58bab75e392e
rakin406 <rakinhacker@gmail.com>
2021-04-01 11:52:07 +0600

    Get CLI argument

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
-// TODO: Get CLI argument
+use std::env;
+use std::process;
+
 fn main() {
+    let args: Vec<_> = env::args().collect();
     let vowels = "aeiou";
     let word = "HELLO";
     let mut syllables = 0;
+
+    // Process CLI argument
+    if args.len() > 1 {
+        println!("The first argument is {}", args[1]);
+    } else {
+        println!("Give argument");
+        process::exit(1);
+    }
 
     // Find vowels in word
     for c in vowels.chars() {
