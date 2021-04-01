281f928ea35b139841dbb62bf7647036ed1e410f
rakin406 <rakinhacker@gmail.com>
2021-04-01 11:40:53 +0600

    Find vowels/syllables

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,14 @@
 fn main() {
     let vowels = "aeiou";
     let word = "Hello";
+    let mut syllables = 0;
+
+    // Find vowels in word
+    for c in vowels.chars() {
+        if word.contains(c) {
+            syllables = syllables + 1;
+        }
+    }
+
+    println!("{}", syllables);
 }
