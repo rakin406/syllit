2a3deb32c7e242e31c866f30e8e506d2f5543de0
rakin406 <rakinhacker@gmail.com>
2021-04-02 14:04:57 +0600

    More tests

--- tests/syllable.rs
+++ tests/syllable.rs
@@ -2,5 +2,10 @@ use syllit::count_syllables;
 
 #[test]
 fn match_syllable() {
-    assert_eq!(count_syllables("Batman"), 2);
+    assert_eq!(count_syllables("Take"), 1);
+    assert_eq!(count_syllables("Bee"), 1);
+    assert_eq!(count_syllables("Taking"), 2);
+    assert_eq!(count_syllables("Redo"), 2);
+    assert_eq!(count_syllables("Dusted"), 2);
+    assert_eq!(count_syllables("Worrying"), 3);
 }
